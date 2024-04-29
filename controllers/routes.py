46903d7d289a513a61cd45b7daca7b249e054988
Diogo Fatec <diogo.kawaguchi.rosa@gmail.com>
2024-04-29 20:42:38 -0300

    Criado as rotas para o cadastro do consumidor e do vendedor

--- controllers/routes.py
+++ controllers/routes.py
@@ -9,3 +9,11 @@ def init_app(app):
     @app.route('/cadastro')
     def cadastro():
         return render_template('cadastro.html')
+    
+    @app.route('/cadastro/consumidor')
+    def cadastro_consumidor():
+        return render_template('cadastro_consumidor.html')
+    
+    @app.route('/cadastro/vendedor')
+    def cadastro_vendedor():
+        return render_template('cadastro_vendedor.html')
