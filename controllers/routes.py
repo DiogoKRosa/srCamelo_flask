b5379eeef96113e1f14af533d1ff6cf324ad226b
Diogo Fatec <diogo.kawaguchi.rosa@gmail.com>
2024-04-29 20:29:01 -0300

    Criou-se a tela 'cadastro.html' para selecionar o tipo de cadastro e inseriu o ícone da página web

--- controllers/routes.py
+++ controllers/routes.py
@@ -5,3 +5,7 @@ def init_app(app):
     @app.route('/')
     def index():
         return render_template('index.html')
+    
+    @app.route('/cadastro')
+    def cadastro():
+        return render_template('cadastro.html')
