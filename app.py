16856eeff4231c9c96d641c7a0f69702b65ff01c
Diogo Fatec <diogo.kawaguchi.rosa@gmail.com>
2024-05-14 19:12:44 -0300

    feito a conexão com o mongoDB e criado a coleção no mongo

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 #Importando as denpendências que serão utilizadas
 from flask import Flask, render_template
 from controllers import routes
+from models.database import mongo, Usuario
 
 # Carregando o Flask na variável app
 app = Flask(__name__, template_folder='views')
-
 routes.init_app(app)
+
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/games'
 
 # Se for executado diretamente pelo interpretador
 if __name__ == '__main__':
+    mongo.init_app(app=app)
+    with app.app_context():
+        if 'usuarios' not in mongo.db.list_collection_names():
+            usuario = Usuario(
+                tipo = '',
+                nome = ''
+            )
+            usuario.save()
+
     app.run(host='localhost', port=5000, debug=True)
---
+++ models/database.py
@@ -0,0 +1,29 @@
+from flask_pymongo import PyMongo
+from bson import ObjectId
+
+mongo = PyMongo()
+
+class Usuario():
+    def __init__(self, tipo, nome, cpf, email, telefone, senha, pais, uf, cidade):
+        self.tipo = tipo
+        self.nome = nome
+        self.cpf = cpf
+        self.email = email
+        self.telefone = telefone
+        self.senha = senha
+        self.pais = pais
+        self.uf = uf
+        self.cidade = cidade
+    
+    def save(self):
+        mongo.db.usuarios.insert_one({
+            'tipo': self.tipo,
+            'nome': self.nome,
+            'cpf': self.cpf,
+            'email': self.email,
+            'telefone': self.telefone,
+            'senha': self.senha,
+            'pais': self.pais,
+            'uf': self.uf,
+            'cidade': self.cidade
+        })
