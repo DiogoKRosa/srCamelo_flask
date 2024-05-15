98d4a1f79e1e1f7d87f3c53df385d564d8e9ae02
Diogo Fatec <diogo.kawaguchi.rosa@gmail.com>
2024-05-14 21:38:29 -0300

    validação de campos vazios arrumado

--- app.py
+++ app.py
@@ -7,7 +7,17 @@ from models.database import mongo, Usuario
 app = Flask(__name__, template_folder='views')
 routes.init_app(app)
 
+# Define a conecão do banco e o nome
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/srcamelo'
+
+# Definindo a secret key para as flash messages
+app.config['SECRET_KEY'] = 'srcamelo'
+
+# Define o tempo de duração da sessão
+app.config['PERMANENT_SESSION_LIFETIME'] = 1800
+
+#Define pasta que receberá arquivos de upload
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
 # Se for executado diretamente pelo interpretador
 if __name__ == '__main__':
--- controllers/routes.py
+++ controllers/routes.py
@@ -1,5 +1,8 @@
 from flask import render_template, request, redirect, url_for
 from models.database import Usuario
+from markupsafe import Markup
+from werkzeug.security import generate_password_hash, check_password_hash
+
 def init_app(app):
 
     @app.route('/')
@@ -18,13 +21,14 @@ def init_app(app):
             if(conferirEmail):
                 return redirect(url_for('cadastro_consumidor'))
             
+            hashed_password = generate_password_hash(request.form['senha'], method='scrypt')
             novoUsu = Usuario(
                 tipo = 'Cliente',
                 nome = request.form['nome'],
                 cpf = request.form['cpf'],
                 email = request.form['email'],
                 telefone = request.form['telefone'],
-                senha = request.form['senha'],
+                senha = hashed_password,
                 pais = request.form['pais'],
                 uf = request.form['uf'],
                 cidade = request.form['cidade'],
